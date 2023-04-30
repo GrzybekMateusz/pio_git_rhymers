1d67a61c9f007bf6417010c47338bca0e9b881ab
GrzybekMateusz <240678@edu.p.lodz.pl>
2023-04-30 14:22:20 +0200

    9. Zmiana modyfikatorów atrybutów klas dla najmniejszej widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int NUMBERS_QUANTITY = 12;
-    public static final int EMPTY_RHYMER_INDICATOR = -1;
-    public static final int FULL_RHYMER_INDICATOR = NUMBERS_QUANTITY - 1;
-    public static final int RETURN_VAL_IF_EMPTY = -1;
+    private static final int NUMBERS_QUANTITY = 12;
+    private static final int EMPTY_RHYMER_INDICATOR = -1;
+    private static final int FULL_RHYMER_INDICATOR = NUMBERS_QUANTITY - 1;
+    private static final int RETURN_VAL_IF_EMPTY = -1;
     private final int[] numbers = new int[NUMBERS_QUANTITY];
 
-    public int total = EMPTY_RHYMER_INDICATOR;
+    private int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
