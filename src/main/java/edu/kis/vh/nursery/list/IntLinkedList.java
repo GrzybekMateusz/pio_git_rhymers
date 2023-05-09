193829062f51d4b6c7c0a2061cc1873d82340c2b
GrzybekMateusz <240678@edu.p.lodz.pl>
2023-05-10 00:47:38 +0200

    17. Dodanie szkieletu dokumentacji dla klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Implementation of linked list containing integers.
+ */
 public class IntLinkedList {
 
     public static final int LIST_EMPTY_INT = -1;
@@ -8,6 +11,10 @@ public class IntLinkedList {
     Node last;
     int i;
 
+    /**
+     * Adds an integer to linked list.
+     * @param i integer to be added to linked list.
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -18,20 +25,39 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Checks if linked list is empty.
+     * @return boolean value indicating that linked list is empty or not.
+     */
     public boolean isEmpty() {
         return last == LIST_EMPTY;
     }
-    
+
+    /**
+     * Checks if linked list is full.
+     * @return boolean value indicating that linked list is not full.
+     */
     public boolean isFull() {
         return LIST_NOT_FULL;
     }
 
+    /**
+     * Returns value of last added number to the linked list.
+     * @return last added number to linked list
+     *         or -1 if list is empty.
+     */
     public int top() {
         if (isEmpty())
             return LIST_EMPTY_INT;
         return last.getValue();
     }
 
+    /**
+     * Returns value of last added number to the linked list
+     * and removes it from the linked list.
+     * @return last added number to linked list
+     *         or -1 if list is empty.
+     */
     public int pop() {
         if (isEmpty())
             return LIST_EMPTY_INT;
@@ -40,32 +66,61 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Implementation of linked list's node storing integer number
+     * and references to next and previous nodes.
+     */
     public class Node {
 
         private final int value;
         private Node prev;
         private Node next;
 
+        /**
+         * Creates new node with
+         * stored value defined by the parameter.
+         * @param i integer value to be stored in node.
+         */
         public Node(int i) {
             value = i;
         }
 
+        /**
+         * Returns value stored in node.
+         * @return value stored in node.
+         */
         public int getValue() {
             return value;
         }
 
+        /**
+         * Gets reference to previous node.
+         * @return reference to previous node.
+         */
         public Node getPrev() {
             return prev;
         }
 
+        /**
+         * Sets reference to previous node.
+         * @param prev reference to previous node.
+         */
         public void setPrev(Node prev) {
             this.prev = prev;
         }
 
+        /**
+         * Returns reference to next node.
+         * @return reference to next node.
+         */
         public Node getNext() {
             return next;
         }
-
+        
+        /**
+         * Sets reference to next Node.
+         * @param next reference to next Node.
+         */
         public void setNext(Node next) {
             this.next = next;
         }
